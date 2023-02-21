78302b313ede6101c050d5738219ef8995479a47
emmanuelangelo <chinonsomaduka2000@gmail.com>
2023-02-21 19:08:39 +0100

    build variable

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
